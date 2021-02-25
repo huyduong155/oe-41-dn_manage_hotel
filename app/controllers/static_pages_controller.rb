5e49f9bc5632c94b5a82b0daa0cf9bdd3452b04d
hieufix1710 <hieufix1710@gmail.com>
2021-02-25 10:55:50 +0700

    static-page-desgin

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def home; end
+
+  def rooms; end
+
+  def about; end
+
+  def gallery; end
+end
--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 require "test_helper"
+=======
+# frozen_string_literal: true
+
+require 'test_helper'
+>>>>>>> static-page-desgin
 
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
   # test "connects with cookies" do
