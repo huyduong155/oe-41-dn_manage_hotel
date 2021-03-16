44e86a8ebc38c91cc1902ba17a7e02cb0cc9d3ff
huyduong155 <nhathuy.155@gmail.com>
2021-03-16 14:37:18 +0700

    create model

---
+++ db/migrate/20210316081830_create_rooms.rb
@@ -0,0 +1,14 @@
+class CreateRooms < ActiveRecord::Migration[6.1]
+  def change
+    create_table :rooms do |t|
+      t.string :name
+      t.float :price
+      t.string :image
+      t.integer :deleted, default: 0
+      t.string :description
+      t.references :room_type, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
